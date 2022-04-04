8f3e38109d9246843977d3786bcdea5233ad2af9
Teddy Blanco <tblanco@student.42quebec.com>
2022-04-04 17:08:31 -0400

    :construction: remove worflow status testing, is tested in real worflow

--- truffleLearn/test/2_TestVoting.js
+++ truffleLearn/test/2_TestVoting.js
@@ -26,6 +26,7 @@ contract.only('Voting', accounts => {
 	const noVoter = accounts[8];
 	const unknow = accounts[9];
 	let VI;
+	/*
 	const status = [{
 			name: 'RegisteringVoters',},
 		{
@@ -49,6 +50,7 @@ contract.only('Voting', accounts => {
 			fn: () => VI.tallyVotes({from: admin}),
 			revMess: "Current status is not voting session ended",}
 	]
+	*/
 
 	const funcs = [{
 			name: 'addVoter',
@@ -109,24 +111,24 @@ contract.only('Voting', accounts => {
 		context("Visibilities of functions and variables", () => {
 			describe("Public and External", () => {
 				it("winningProposalID",	() => assert.isDefined(VI.winningProposalID));
-				it("workflowsStatus",		() => assert.isDefined(VI.workflowStatus));
-				it("proposalsArray",		() => assert.isDefined(VI.proposalsArray));
-				it("getVoter",					() => assert.isDefined(VI.getVoter));
-				it("getOneProposal",		() => assert.isDefined(VI.getOneProposal));
-				it("addVoter",					() => assert.isDefined(VI.addVoter));
-				it("addProposal",				() => assert.isDefined(VI.addProposal));
-				it("setVote",						() => assert.isDefined(VI.setVote));
+				it("workflowsStatus",	() => assert.isDefined(VI.workflowStatus));
+				it("proposalsArray",	() => assert.isDefined(VI.proposalsArray));
+				it("getVoter",			() => assert.isDefined(VI.getVoter));
+				it("getOneProposal",	() => assert.isDefined(VI.getOneProposal));
+				it("addVoter",			() => assert.isDefined(VI.addVoter));
+				it("addProposal",		() => assert.isDefined(VI.addProposal));
+				it("setVote",			() => assert.isDefined(VI.setVote));
 				
 				it("startProposalsRegistering",	() => assert.isDefined(VI.startProposalsRegistering));
-				it("endProposalsRegistering",		() => assert.isDefined(VI.endProposalsRegistering));
-				it("startVotingSession",				() => assert.isDefined(VI.startVotingSession));
-				it("endVotingSession",					() => assert.isDefined(VI.endVotingSession));
-				it("tallyVotes",						() => assert.isDefined(VI.tallyVotes));
+				it("endProposalsRegistering",	() => assert.isDefined(VI.endProposalsRegistering));
+				it("startVotingSession",		() => assert.isDefined(VI.startVotingSession));
+				it("endVotingSession",			() => assert.isDefined(VI.endVotingSession));
+				it("tallyVotes",				() => assert.isDefined(VI.tallyVotes));
 			})
 			describe("Private and Internal", () => {
 				it("winningProposalsID",	() => assert.isUndefined(VI.winningProposalsID));
 				it("winningProposals",		() => assert.isUndefined(VI.winningProposals));
-				it("voters",							() => assert.isUndefined(VI.voters));
+				it("voters",				() => assert.isUndefined(VI.voters));
 			})
 		})
 		context("Modifiers accessibility in functions", () => {
@@ -161,6 +163,7 @@ contract.only('Voting', accounts => {
 			})
 		})
 	})
+	/*
 	context("Testing workflowStatus chagement", () => {
 		before(async () => {
 			VI = await Voting.new({from: admin});
@@ -186,11 +189,14 @@ contract.only('Voting', accounts => {
 			})
 		}
 	})
+	*/
 	context("test des fonction pendant un cas reel", () => {
 		before(async () => {
 			VI = await Voting.new({from: admin});
 		})
 		context("While RegisteringVoters status:", () =>{
+			it(`Current status shoulb be 0`, 	async () =>
+					expect_equal_BN(await VI.workflowStatus({from: admin}), 0));
 			describe("adding voters {from: admin}:", () => {
 				for (let voter_id = 0; voter_id < voters.length; voter_id++){
 					it(`addVoter(${voters[voter_id].at}), event VoterRegistered shoulb be emit ${voters[voter_id].at}`,
@@ -212,6 +218,8 @@ contract.only('Voting', accounts => {
 			it(`changing status to ProposalsRegistrationStarted with startProposalsRegistering({from: admin}) event should be (0, 1)`,
 				async () => expectEvent(await VI.startProposalsRegistering({from: admin}), 'WorkflowStatusChange',
 				{previousStatus: new BN(0), newStatus: new BN(1)}))
+			it(`Current status shoulb be 1`, 	async () =>
+				expect_equal_BN(await VI.workflowStatus({from: admin}), 1));
 			describe(`adding proposals`, () =>{		
 				for (let voter_id = 0; voter_id < voters.length - 1; voter_id++){
 					it(`addProposal(${voters[voter_id].prop}, {from: ${voters[voter_id].at}}, event ProposalRegistered shoulb be emit ${voter_id}`,
@@ -235,12 +243,16 @@ contract.only('Voting', accounts => {
 			it(`changing status to ProposalsRegistrationEnded with endProposalsRegistering({from: admin}) event should be (1, 2)`,
 				async () => expectEvent(await VI.endProposalsRegistering({from: admin}), 'WorkflowStatusChange',
 				{previousStatus: new BN(1), newStatus: new BN(2)}))
+			it(`Current status shoulb be 2`, 	async () =>
+				expect_equal_BN(await VI.workflowStatus({from: admin}), 2));
 			describe(`functions can't call with the current status`, () => tryFunctions(2))
 		})
 		context(`While VotingSessionStarted status`, () =>{
 			it(`changing status to VotingSessionStarted with startVotingSession({from: admin}) event should be (0, 1)`,
 				async () => expectEvent(await VI.startVotingSession({from: admin}), 'WorkflowStatusChange',
 				{previousStatus: new BN(2), newStatus: new BN(3)}))
+			it(`Current status shoulb be 3`, 	async () =>
+				expect_equal_BN(await VI.workflowStatus({from: admin}), 3));
 			describe(`adding votes`, () =>{		
 				for (let voter_id = 0; voter_id < voters.length - 1; voter_id++){
 					it(`setVote(${voters[voter_id].vote}, {from: ${voters[voter_id].at}}, event Voted shoulb be emit (${voters[voter_id].at}, ${voters[voter_id].vote})`,
@@ -291,12 +303,16 @@ contract.only('Voting', accounts => {
 			it(`changing status to VotingSessionEnded with endVotingSession({from: admin}) event should be (3, 4)`,
 				async () => expectEvent(await VI.endVotingSession({from: admin}), 'WorkflowStatusChange',
 				{previousStatus: new BN(3), newStatus: new BN(4)}))
+			it(`Current status shoulb be 4`, 	async () =>
+				expect_equal_BN(await VI.workflowStatus({from: admin}), 4));
 			describe(`functions can't call with the current status`, () => tryFunctions(4))
 		})
 		context(`While VotesTallied status`, () =>{
 			it(`changing status to VotesTallied with tallyVotes({from: admin}) event should be (4, 5)`,
 				async () => expectEvent(await VI.tallyVotes({from: admin}), 'WorkflowStatusChange',
 				{previousStatus: new BN(4), newStatus: new BN(5)}))
+			it(`Current status shoulb be 5`, 	async () =>
+				expect_equal_BN(await VI.workflowStatus({from: admin}), 5));
 			it(`winningProposalID() should be return 2`, async () =>
 				expect_equal_BN(await VI.winningProposalID({from: admin}), 2));
 			describe(`functions can't call with the current status`, () => tryFunctions(5))
--- truffleLearn/test/README.md
+++ truffleLearn/test/README.md
@@ -15,6 +15,7 @@
 
 ## Todo:
 - [ ] Readme
-- [ ] Remove the testing of all status (already test with `tryFcuntions()`), just add the current status check in each test
+- [x] Remove the testing of all status (already test with `tryFcuntions()`), just add the current status check in each test
+- [ ] Refactor status number by a value in each context.
 - [ ] improve message in each `context`, `describe`, `it`
 - [ ] change assert by expect
